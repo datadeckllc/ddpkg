70ecdf31685c27d6ebb80095e8f18089aa7f1668
Tyler Curtis Jowers <jowerstc@gmail.com>
2020-07-26 21:40:21 -0700

    changed to uppercase

---
+++ tps/tps.go
@@ -0,0 +1,22 @@
+package tps
+
+import (
+  "fmt"
+  "net/http"
+  "log"
+  "golang.org/x/net/html"
+)
+
+const TPS_URL := "https://truepeoplesearch.com/"
+
+func Tps_search(fname, lname, mname string){
+  var tps_str string = html.EscapeString(fname + " " +  mname + " " +lname)
+  res, e := http.Get(TPS_URL + "results?=" + tps_str)
+  e != nil {
+    log.Fatal(e)
+  }
+
+  defer tps.Body.Close()
+  doc, e := html.Parse(res)
+  fmt.Printf("%T\n", )
+}
